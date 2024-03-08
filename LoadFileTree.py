f9316113650efff79595a2f4d3fdf381fb64eaf9
fishyy119 <1830979240@qq.com>
2024-03-08 13:16:19 +0800

    增加黑名单功能

--- LoadFileTree.py
+++ LoadFileTree.py
@@ -1,13 +1,18 @@
 import os
 import json
-
+'''
+todo:
+增加重名歌曲报告功能
+'''
 class FileTreeMatcher:
     def __init__(self,
                  root_dir: str,
-                 white_extension: list
+                 white_extension: list,
+                 black_song: list
                  ) -> None:
         self.root_dir = root_dir  # 存储音乐库的根目录
         self.white_extension = white_extension
+        self.black_song = black_song
         self.file_tree = {}
 
     def build_file_tree(self) -> None:
@@ -80,6 +85,9 @@ class FileTreeMatcher:
                 new_path_nn = new_path + "\\" + name  # 用于规避引用传递
                 self._compare_file_trees(new_node, old_tree, path_mapping, new_path_nn)
             else:  # 如果是文件节点
+                if name + new_node in self.black_song:  # 黑名单跳过匹配
+                    continue
+
                 old_path, extension = self._find_file_in_tree(name, old_tree, '')
                 if old_path is not None:  # 如果在旧文件树中存在相同文件
                     if old_path[0] == "\\":
@@ -109,9 +117,11 @@ class FileTreeMatcher:
     
 class SettingLoader:
     DEFAULT_SETTINGS = {
-        'root_dir': '/path/to/root/directory',
+        'root_dir': '/path/to/songs/root/directory',
         'm3u_directory': '/path/to/m3u/files',
-        'white_extension': ['.mp3', '.wav', '.flac']
+        'white_extension': ['.mp3', '.wav', '.flac'],
+        'black_list_state': False,
+        'black_song': ['sample.mp3']
     }
 
     def __init__(self, 
@@ -124,9 +134,10 @@ class SettingLoader:
             with open(self.filename, 'r', encoding='utf-8') as f:
                 settings = json.load(f)
         except FileNotFoundError:
-            print(f"Settings file '{self.filename}' not found. Creating default settings.")
+            print(f"未发现设置文件'{self.filename}'，已自动创建，请前往设置")
             settings = self.DEFAULT_SETTINGS
             self.save_settings(settings)
+            exit(0)
         return settings
 
     def save_settings(self, 
@@ -141,17 +152,22 @@ if __name__ == "__main__":
     root_dir = settings['root_dir']  # 音乐库文件
     m3u_directory = settings['m3u_directory']  # m3u存储路径
     white_extension = settings['white_extension']  # 音乐后缀名识别
+    black_list_state = settings['black_list_state']  # 黑名单开关
+    if black_list_state:
+        black_song = settings['black_song']  # 跳过匹配的歌曲黑名单
+    else:
+        black_song = []
 
-    file_tree_matcher_old = FileTreeMatcher(root_dir, white_extension)
+    file_tree_matcher_old = FileTreeMatcher(root_dir, white_extension, black_song)
     file_tree_matcher_old.build_file_tree_from_m3u(m3u_directory)
     file_tree_matcher_old.save_to_json("file_tree_m3u.json")
 
-    file_tree_matcher = FileTreeMatcher(root_dir, white_extension)
+    file_tree_matcher = FileTreeMatcher(root_dir, white_extension, black_song)
     file_tree_matcher.build_file_tree()
     file_tree_matcher.save_to_json("file_tree.json")
 
     match_map = file_tree_matcher.get_path_mapping(file_tree_matcher_old.file_tree)
     with open('map.json', 'w', encoding='utf-8') as f:
-            print(f"匹配完毕，共匹配到{len(match_map)}项")
-            f.write(json.dumps(match_map, ensure_ascii=False, indent=4))
+        print(f"匹配完毕，共匹配到{len(match_map)}项")
+        f.write(json.dumps(match_map, ensure_ascii=False, indent=4))
     
