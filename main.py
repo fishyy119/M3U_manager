067710e1707301ef5861603789c27d0265f9bee5
fishyy119 <1830979240@qq.com>
2024-05-01 22:12:25 +0800

    优化可读性

--- main.py
+++ main.py
@@ -14,63 +14,48 @@ class M3UMManagerApp:
         self.setting_loader = SettingLoader("setting.json")
         self.settings = self.setting_loader.read_settings()
         
-        self.root.minsize(600, 400)
-        self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(2, weight=1)
-        # 对于中间的按钮列，该行留空
-        # 此行用于吸收纵向的多余空间
-        self.root.grid_rowconfigure(2, weight=1)  
-        
+        ############################################################################
         # Frame：显示信息
         info_frame = tk.Frame(root, bd=2, relief=tk.GROOVE)
-        info_frame.grid(row=0, column=1, sticky='ew', padx=10, pady=10)
         
         self.m3u_info = tk.Label(info_frame)
-        self.m3u_info.pack()
         self.playlist_info = tk.Label(info_frame)
-        self.playlist_info.pack()
         
+        self.m3u_info.pack()  # m3u个数
+        self.playlist_info.pack()  # 歌曲个数
+        
+        ############################################################################
         # Frame：容纳刷新、创建两个按钮
         refresh_create_frame = tk.Frame(root, bd=2, relief=tk.GROOVE)
-        refresh_create_frame.grid(row=3, column=1, sticky='ews', padx=10, pady=10)
         
-        # 按钮：刷新
         refresh_button = tk.Button(refresh_create_frame, text="刷新", command=self.refresh_directory)
-        refresh_button.pack(padx=10, pady=10, ipadx=5, side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
-        # 按钮：创建新m3u文件
         self.open_child_window_button = tk.Button(refresh_create_frame, text="创建新m3u文件", command=self.open_merge_m3u_window)
-        self.open_child_window_button.pack(padx=10, pady=10, ipadx=5, side=tk.RIGHT, fill=tk.BOTH, expand=True)
         
+        refresh_button.pack(padx=10, pady=10, ipadx=5, side=tk.LEFT, fill=tk.BOTH, expand=True)  # 刷新
+        self.open_child_window_button.pack(padx=10, pady=10, ipadx=5, side=tk.RIGHT, fill=tk.BOTH, expand=True)  # 创建新m3u文件
+        
+        ############################################################################
         # Frame：容纳打乱、去重、上移、下移、置顶、删除六个按钮
         editm3u_frame = tk.Frame(root, bd=2, relief=tk.GROOVE, width=40)
-        editm3u_frame.grid(row=1, column=1, sticky='ew', padx=10, pady=10)
         
         label = tk.Label(editm3u_frame, text="操作右侧列表：")
-        label.grid(row=1, column=1, columnspan=4, pady=(0,10))
-        
-        # 按钮：打乱播放列表
         shuffle_button = tk.Button(editm3u_frame, text="随机打乱", command=self.shuffle_playlist)
-        shuffle_button.grid(row=3, column=1, columnspan=2, padx=10, pady=(0,10), ipadx=5, sticky='ew')
-        
-        # 按钮：播放列表去重
         deduplicate_button = tk.Button(editm3u_frame, text="去除重复项", command=self.deduplicate_playlist)
-        deduplicate_button.grid(row=3, column=3, columnspan=2, padx=10, pady=(0,10), ipadx=5, sticky='ew')
-        
-        # 按钮：对列表中歌曲的上移、下移、置顶、删除
         self.btn_move_up = tk.Button(editm3u_frame, text="上移", command=self.move_up)
-        self.btn_move_up.grid(row=2, column=1, padx=5, pady=10, ipadx=5)
-        
         self.btn_move_down = tk.Button(editm3u_frame, text="下移", command=self.move_down)
-        self.btn_move_down.grid(row=2, column=2, padx=5, pady=10, ipadx=5)
-        
         self.btn_move_top = tk.Button(editm3u_frame, text="置顶", command=self.move_top)
-        self.btn_move_top.grid(row=2, column=3, padx=5, pady=10, ipadx=5)
-        
         self.btn_delete = tk.Button(editm3u_frame, text="删除", command=self.delete)
-        self.btn_delete.grid(row=2, column=4, padx=5, pady=10, ipadx=5)
         
+        label.grid(row=1, column=1, columnspan=4, pady=(0,10))
+        shuffle_button.grid(row=3, column=1, columnspan=2, padx=10, pady=(0,10), ipadx=5, sticky='ew')  # 随机打乱
+        deduplicate_button.grid(row=3, column=3, columnspan=2, padx=10, pady=(0,10), ipadx=5, sticky='ew')  # 去重
+        self.btn_move_up.grid(row=2, column=1, padx=5, pady=10, ipadx=5)  # 上移
+        self.btn_move_down.grid(row=2, column=2, padx=5, pady=10, ipadx=5)  # 下移
+        self.btn_move_top.grid(row=2, column=3, padx=5, pady=10, ipadx=5)  # 置顶
+        self.btn_delete.grid(row=2, column=4, padx=5, pady=10, ipadx=5)  # 删除
         
+        ############################################################################
+        # Lisbbox相关 
         # 存储m3u文件的路径(与m3u_listbox同序)
         self.m3u_path_list = []
         
@@ -78,13 +63,27 @@ class M3UMManagerApp:
         self.m3u_listbox = tk.Listbox(root, width=60, height=30)
         # 无下划线 失去焦点选中不变 选中颜色
         self.m3u_listbox.configure(activestyle='none', exportselection=False, selectbackground='green')
-        self.m3u_listbox.grid(row=0, column=0, rowspan=4, sticky="nsew", padx=10, pady=10)
         self.m3u_listbox.bind("<<ListboxSelect>>", self.show_selected_playlist)
         
         # 列表框：歌曲
         self.song_listbox = tk.Listbox(root, width=60, height=30)
         self.song_listbox.configure(activestyle='none', exportselection=False, selectbackground='green')
+        
+        ############################################################################
+        # 主窗口布局
+        self.root.minsize(600, 400)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(2, weight=1)
+        # 对于中间的按钮列，该行留空
+        # 此行用于吸收纵向的多余空间
+        self.root.grid_rowconfigure(2, weight=1)  
+        
+        self.m3u_listbox.grid(row=0, column=0, rowspan=4, sticky="nsew", padx=10, pady=10)
         self.song_listbox.grid(row=0, column=2, rowspan=4, sticky="nsew", padx=10, pady=10)
+        info_frame.grid(row=0, column=1, sticky='ew', padx=10, pady=10)
+        editm3u_frame.grid(row=1, column=1, sticky='ew', padx=10, pady=10)
+        refresh_create_frame.grid(row=3, column=1, sticky='ews', padx=10, pady=10)
+        ############################################################################
         
         # 加载播放列表文件
         self.load_playlist_files()
