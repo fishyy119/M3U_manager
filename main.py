3b402f09fdeb67cd6b05ca50471d9351cde54978
fishyy119 <1830979240@qq.com>
2024-05-02 23:14:35 +0800

    重命名；超链接

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import os
 import random
 import json
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog, messagebox
 from SettingLoader import SettingLoader
 from MergeM3UWindow import MergeM3UWindow
-
+"""
+todo:
+    打开库
+    设置界面
+    整合12345
+"""
 
 class M3UMManagerApp:
-    def __init__(self, root):
+    def __init__(self, root: tk.Tk):
         self.root = root
         self.root.title("M3U Manager")
+        self.root.iconbitmap('img/favicon.ico')
         self.setting_loader = SettingLoader("setting.json")
         self.settings = self.setting_loader.read_settings()
         
@@ -25,16 +31,21 @@ class M3UMManagerApp:
         self.playlist_info.pack()  # 歌曲个数
         
         ############################################################################
-        # Frame：容纳刷新、删除、创建
+        # Frame：容纳刷新、删除、创建、重命名
         refresh_create_frame = tk.Frame(root, bd=2, relief=tk.GROOVE)
         
         refresh_button = tk.Button(refresh_create_frame, text="刷新", command=self.refresh_directory)
         delete_m3u_button = tk.Button(refresh_create_frame, text="删除m3u", command=self.delete_m3u)
+        rename_m3u_button = tk.Button(refresh_create_frame, text="重命名m3u", command=self.rename_m3u)
         self.open_child_window_button = tk.Button(refresh_create_frame, text="创建m3u", command=self.open_merge_m3u_window)
         
-        refresh_button.pack(padx=10, pady=10, ipadx=5, side=tk.LEFT, fill=tk.BOTH, expand=True)  # 刷新
-        delete_m3u_button.pack(padx=10, pady=5, ipadx=5, side=tk.TOP, fill=tk.BOTH, expand=True)  # 删除m3u
-        self.open_child_window_button.pack(padx=10, pady=5, ipadx=5, side=tk.BOTTOM, fill=tk.BOTH, expand=True)  # 创建新m3u文件
+        refresh_create_frame.grid_columnconfigure(0, weight=1)
+        refresh_create_frame.grid_columnconfigure(1, weight=1)
+        
+        refresh_button.grid(row=0, column=0, padx=10, pady=5, sticky='ew')  # 刷新
+        delete_m3u_button.grid(row=0, column=1, padx=10, pady=5, sticky='ew')  # 删除m3u
+        rename_m3u_button.grid(row=1, column=0, padx=10, pady=5, sticky='ew')  # 重命名m3u
+        self.open_child_window_button.grid(row=1, column=1, padx=10, pady=5, sticky='ew')  # 创建新m3u文件
         
         ############################################################################
         # Frame：容纳打乱、去重、上移、下移、置顶、删除六个按钮
@@ -43,18 +54,18 @@ class M3UMManagerApp:
         label = tk.Label(editm3u_frame, text="操作右侧列表：")
         shuffle_button = tk.Button(editm3u_frame, text="随机打乱", command=self.shuffle_playlist)
         deduplicate_button = tk.Button(editm3u_frame, text="去除重复项", command=self.deduplicate_playlist)
-        self.btn_move_up = tk.Button(editm3u_frame, text="上移", command=self.move_up)
-        self.btn_move_down = tk.Button(editm3u_frame, text="下移", command=self.move_down)
-        self.btn_move_top = tk.Button(editm3u_frame, text="置顶", command=self.move_top)
-        self.btn_delete = tk.Button(editm3u_frame, text="删除", command=self.delete)
+        btn_move_up = tk.Button(editm3u_frame, text="上移", command=self.move_up)
+        btn_move_down = tk.Button(editm3u_frame, text="下移", command=self.move_down)
+        btn_move_top = tk.Button(editm3u_frame, text="置顶", command=self.move_top)
+        btn_delete = tk.Button(editm3u_frame, text="删除", command=self.delete)
         
-        label.grid(row=1, column=1, columnspan=4, pady=(0,10))
+        label.grid(row=1, column=1, columnspan=4, pady=(2,5))
         shuffle_button.grid(row=3, column=1, columnspan=2, padx=10, pady=(0,10), ipadx=5, sticky='ew')  # 随机打乱
         deduplicate_button.grid(row=3, column=3, columnspan=2, padx=10, pady=(0,10), ipadx=5, sticky='ew')  # 去重
-        self.btn_move_up.grid(row=2, column=1, padx=5, pady=10, ipadx=5)  # 上移
-        self.btn_move_down.grid(row=2, column=2, padx=5, pady=10, ipadx=5)  # 下移
-        self.btn_move_top.grid(row=2, column=3, padx=5, pady=10, ipadx=5)  # 置顶
-        self.btn_delete.grid(row=2, column=4, padx=5, pady=10, ipadx=5)  # 删除
+        btn_move_up.grid(row=2, column=1, padx=5, pady=10, ipadx=5)  # 上移
+        btn_move_down.grid(row=2, column=2, padx=5, pady=10, ipadx=5)  # 下移
+        btn_move_top.grid(row=2, column=3, padx=5, pady=10, ipadx=5)  # 置顶
+        btn_delete.grid(row=2, column=4, padx=5, pady=10, ipadx=5)  # 删除
         
         ############################################################################
         # Lisbbox相关 
@@ -84,10 +95,19 @@ class M3UMManagerApp:
         self.song_listbox.grid(row=0, column=2, rowspan=4, sticky="nsew", padx=10, pady=10)
         info_frame.grid(row=0, column=1, sticky='ew', padx=10, pady=10)
         editm3u_frame.grid(row=1, column=1, sticky='ew', padx=10, pady=10)
-        refresh_create_frame.grid(row=3, column=1, sticky='ews', padx=10, pady=10)
+        refresh_create_frame.grid(row=3, column=1, rowspan=2, sticky='ews', padx=10, pady=10)
+        ############################################################################
+        # 添加超链接
+        link_m3u_label = tk.Label(self.root, text="打开m3u库", fg="blue", cursor="hand2")
+        link_playlist_label = tk.Label(self.root, text="打开m3u文件", fg="blue", cursor="hand2")
+        link_m3u_label.bind("<Button-1>", self.open_m3u_folder)
+        link_playlist_label.bind("<Button-1>", self.open_playlist)
+        
+        link_m3u_label.grid(row=4, column=0, padx=10, pady=(0,4), sticky="nw")
+        link_playlist_label.grid(row=4, column=2, padx=10, pady=(0,4), sticky="ne")
         ############################################################################
         
-        # 加载播放列表文件
+        # 加载m3u文件
         self.load_playlist_files()
 
     def load_playlist_files(self):
@@ -151,7 +171,12 @@ class M3UMManagerApp:
             random.seed()
             random.shuffle(list_index)
         elif operation == 'deduplicate':
-            list_index = list(set(list_index))
+            tmp_playlist = []  # 用于去重
+            for i, song in enumerate(playlist):
+                if not song in tmp_playlist:
+                    tmp_playlist.append(song)
+                else:
+                    list_index.remove(i)
         elif operation == 'up':
             if index > 0:
                 list_index[index - 1] = index
@@ -245,6 +270,7 @@ class M3UMManagerApp:
         self.merge_m3u_window = MergeM3UWindow(self.root, self.m3u_path_list, self.settings.get("m3u_directory"))
         # 用以解决弹出警告窗口后该窗口被销毁的问题
         self.merge_m3u_window.top.wait_window()
+        self.load_playlist_files()
         
     def refresh_directory(self):
         # 刷新播放列表
@@ -262,6 +288,48 @@ class M3UMManagerApp:
                 os.remove(file_to_delete)
                 # 更新列表框
                 self.refresh_directory()
+                
+    def rename_m3u(self):
+        # 重命名m3u文件
+        selected_index = self.m3u_listbox.curselection()
+        old_path = self.m3u_path_list[selected_index[0]]
+        # 弹出对话框，让用户输入新的文件名
+        new_name = simpledialog.askstring("重命名", "输入新文件名（无需输入后缀名）：")
+
+        # 检查用户是否点击了取消按钮或没有输入任何内容
+        if new_name is None:
+            return
+        elif not new_name.strip():
+            messagebox.showerror("Error", "输入为空")
+            return
+        
+        
+        # 构建新的文件路径
+        directory = os.path.dirname(old_path)
+        new_path = os.path.join(directory, new_name)
+
+        try:
+            # 添加.m3u扩展名
+            if not new_path.endswith(".m3u"):
+                new_path += ".m3u"
+                
+            # 重命名文件
+            os.rename(old_path, new_path)
+        except Exception as e:
+            messagebox.showerror("Error", f"文件重命名失败：{e}")
+            
+        self.load_playlist_files()
+
+    def open_m3u_folder(self, event: tk.Event):
+        # 打开m3u库文件夹
+        event.widget.config(fg='purple4')
+        os.system(f"explorer {self.settings['m3u_directory']}")
+        
+    def open_playlist(self, event: tk.Event):
+        # 打开m3u文件
+        event.widget.config(fg='purple4')
+        file_path = self.m3u_path_list[self.m3u_listbox.curselection()[0]]
+        os.startfile(file_path)
 
 if __name__ == "__main__":
     # 创建 Tkinter 窗口，并启动应用程序
